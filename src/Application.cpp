a63bac66399d84c25b4beb4bbd38542cbf70e73e
awaわ <a970565@163.com>
2024-04-28 21:31:17 +0800

    添加项目文件。

---
+++ src/Application.cpp
@@ -0,0 +1,12 @@
+/*
+This file manage apps
+set your config
+*/
+
+#include "discord/Rult.hpp"
+
+int main() {
+	Rult::Init("/home/awalwa/projects/bot/config.txt");
+	
+	return 9;
+}
---
+++ src/discord/Fn/terminal.cpp
@@ -0,0 +1,8 @@
+#include "../Ru_koto.h"
+
+
+namespace Rult {
+	void Ru_koto::terminal() {
+		
+	}
+}
